cccac3f53ffbc19b121e243dc5e020b6a822796c
Paul1435 <49119046+Paul1435@users.noreply.github.com>
2023-10-06 23:08:28 +0300

    Delete Лабораторная работа 1/task2.py

--- Лабораторная работа 1/task2.py
+++
@@ -1,7 +0,0 @@
-list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-
-medium = len(list_players) // 2
-team_1 = list_players[:medium]
-team_2 = list_players[medium:]
-print(team_1)
-print(team_2)
