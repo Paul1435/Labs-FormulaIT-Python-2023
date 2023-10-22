cad340fd9de698864dc9d11c0688e175beb434fd
Paul1435 <49119046+Paul1435@users.noreply.github.com>
2023-10-22 15:58:10 +0300

    Лабораторная работа3

---
+++ Лабораторная работа 3/task3.py
@@ -0,0 +1,54 @@
+def count_letters(text):
+    letter_counts = {}
+    for char in text:
+        if char.isalpha():
+            lowercase_char = char.lower()
+            letter_counts[lowercase_char] = letter_counts.get(lowercase_char, 0) + 1
+    return letter_counts
+
+
+def calculate_frequency(letter_counts):
+    total_letters = sum(letter_counts.values())
+    frequency_dict = {}
+    for letter, count in letter_counts.items():
+        frequency_dict[letter] = round(count / total_letters, 2)
+    return frequency_dict
+
+
+main_str = """
+У лукоморья дуб зелёный;
+Златая цепь на дубе том:
+И днём и ночью кот учёный
+Всё ходит по цепи кругом;
+Идёт направо — песнь заводит,
+Налево — сказку говорит.
+Там чудеса: там леший бродит,
+Русалка на ветвях сидит;
+Там на неведомых дорожках
+Следы невиданных зверей;
+Избушка там на курьих ножках
+Стоит без окон, без дверей;
+Там лес и дол видений полны;
+Там о заре прихлынут волны
+На брег песчаный и пустой,
+И тридцать витязей прекрасных
+Чредой из вод выходят ясных,
+И с ними дядька их морской;
+Там королевич мимоходом
+Пленяет грозного царя;
+Там в облаках перед народом
+Через леса, через моря
+Колдун несёт богатыря;
+В темнице там царевна тужит,
+А бурый волк ей верно служит;
+Там ступа с Бабою Ягой
+Идёт, бредёт сама собой,
+Там царь Кащей над златом чахнет;
+Там русский дух… там Русью пахнет!
+И там я был, и мёд я пил;
+У моря видел дуб зелёный;
+Под ним сидел, и кот учёный
+Свои мне сказки говорил.
+"""
+for symb, frequency in calculate_frequency(count_letters(main_str)).items():
+    print(f"{symb}: {frequency:.2f}")
